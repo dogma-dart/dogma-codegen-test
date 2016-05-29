eb8864233fe7509f6a2a62c1e11d2bb83d77d503
Don <don.j.olmstead@gmail.com>
2016-05-29 09:03:11 -0700

    Add privacy metadata equality

---
+++ lib/src/metadata/privacy_metadata.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_source_analyzer/metadata.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Checks whether the metadata in [a] and [b] is equal.
+bool privacyMetadataEqual(PrivacyMetadata a, PrivacyMetadata b) =>
+    a.isPrivate == b.isPrivate;
