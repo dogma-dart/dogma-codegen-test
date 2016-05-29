fbc63915116484ba4e2cfbd2a3295526272e91e3
Don <don.j.olmstead@gmail.com>
2016-05-29 09:02:59 -0700

    Add constant metadata equality

---
+++ lib/src/metadata/constant_metadata.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_source_analyzer/metadata.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Checks whether the metadata in [a] and [b] is equal.
+bool constantMetadataEqual(ConstantMetadata a, ConstantMetadata b) =>
+    a.isConst == b.isConst;
