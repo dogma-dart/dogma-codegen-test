86bd8e0d8f2e6a89876ff527d4d1663f567655db
Don <don.j.olmstead@gmail.com>
2016-05-29 09:14:03 -0700

    Add clones

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -ex
 
+# Clone dogma libraries
+git clone https://github.com/dogma-dart/dogma-convert.git ../dogma-convert
+git clone https://github.com/dogma-dart/dogma-source-analyzer.git ../dogma-source-analyzer
+git clone https://github.com/dogma-dart/dogma-union-type.git ../dogma-union-type
+
 # Get version
 dart --version
 
