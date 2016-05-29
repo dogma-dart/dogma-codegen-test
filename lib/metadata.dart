5ddabc40ad2d14e7e769054db83dd4882898abbd
Don <don.j.olmstead@gmail.com>
2016-05-29 09:07:33 -0700

    Expose equality checks for metadata

---
+++ lib/metadata.dart
@@ -0,0 +1,21 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+/// Contains equality tests for metadata.
+library dogma_codegen_test.metadata;
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/metadata/abstract_metadata.dart';
+export 'src/metadata/class_metadata.dart';
+export 'src/metadata/constant_metadata.dart';
+export 'src/metadata/field_metadata.dart';
+export 'src/metadata/library_metadata.dart';
+export 'src/metadata/metadata.dart';
+export 'src/metadata/privacy_metadata.dart';
+export 'src/metadata/static_metadata.dart';
+export 'src/metadata/typed_metadata.dart';
