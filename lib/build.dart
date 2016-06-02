8e3946730985d83b64008532bdadfde483bd3b47
Don <don.j.olmstead@gmail.com>
2016-06-01 23:21:40 -0700

    Add build library

---
+++ lib/build.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+/// Contains helpers for testing builders.
+library dogma_codegen_test.build;
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/build/expected_metadata_builder.dart';
+export 'src/build/predefined_metadata_step.dart';
+export 'src/build/test.dart';
