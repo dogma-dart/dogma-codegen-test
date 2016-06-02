1d7c70670f4d63b409a4afe84070aed3731e037f
Don <don.j.olmstead@gmail.com>
2016-06-01 23:11:22 -0700

    Export convert library metadata

---
+++ lib/convert.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+/// Contains test metadata for converters.
+library dogma_codegen_test.convert;
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/convert/model_implicit_library.dart';
