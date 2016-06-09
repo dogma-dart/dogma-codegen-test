4ef6a187535411e34722547d824d10eba3968adb
Don <don.j.olmstead@gmail.com>
2016-06-08 23:46:44 -0700

    Fix the build hopefully

--- build.sh
+++ build.sh
@@ -15,23 +15,22 @@ pub install
 # Verify that the libraries are error and warning-free.
 dartanalyzer ${DARTANALYZER_FLAGS} $(ls -rt lib/*.dart)
 
-# Clone coverage till repo is fixed
-git clone -b migration https://github.com/dart-lang/coverage.git ../coverage
-
 # Run the tests
-pub global activate --source path ../coverage
+pub global activate coverage
 OBSERVATORY_PORT=8000
-COVERAGE_OUTPUT=lcov.info
+COVERAGE_OUTPUT=coverage.json
 
-pub global run coverage:coverage collect \
+dart --checked --observe=${OBSERVATORY_PORT} test/all.dart & \
+pub global run coverage:collect_coverage \
     --port=${OBSERVATORY_PORT} \
-    --output=${COVERAGE_OUTPUT} \
-    --pause-timeout=120 \
+    --out ${COVERAGE_OUTPUT} \
     --wait-paused \
     --resume-isolates & \
+wait
 
-dart --checked --observe=${OBSERVATORY_PORT} test/all.dart
-
-wait $!
-
-ls
+pub global run coverage:format_coverage \
+    --package-root=packages \
+    --report-on lib \
+    --in ${COVERAGE_OUTPUT} \
+    --out lcov.info \
+    --lcov
---
+++ test/all.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2015-2016 the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+// \TODO Remove this file after https://github.com/dart-lang/test/issues/36 is resolved.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:test/test.dart';
+
+import 'metadata_test.dart' as metadata_test;
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+void main() {
+  group('Metadata tests', metadata_test.main);
+}
